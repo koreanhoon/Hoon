c2fe5503614714ed1fb1ba52f6c46f60297cec15
Koreanhoon <ryanhoonseok@gmail.com>
2022-10-10 16:57:46 +0900

    Initial Commit

---
+++ Hoon/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Hoon
+//
+//  Created by Ryan Lee on 2022/10/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hoon/HoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoonApp.swift
+//  Hoon
+//
+//  Created by Ryan Lee on 2022/10/10.
+//
+
+import SwiftUI
+
+@main
+struct HoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
